731d8c1c0d99186eef251f8826806b22ff6e2b17
Alberto Monti <20albymonti05@gmail.com>
2023-04-02 19:57:06 +0200

    Update CGestioneToDo.js

--- ToDoList/CGestioneToDo.js
+++ ToDoList/CGestioneToDo.js
@@ -30,13 +30,122 @@ class CGestioneToDo
         }
     }
 
-    ToDoNellaTable()
+    visualizzaPerData()
     {
         //ordino il vettore di todo in base alla data
         this.ordinaEventiPerData();
         
         //visualizzo tutti i todo nella table
         this.visualizzaToDoNellaTabella();
+    }
+
+    ToDoNellaTable()
+    {
+        //visualizzo tutti i todo nella table
+        this.visualizzaToDoNellaTabella();
+    }
+
+    visualizzaCompletate()
+    {
+        //creo un vettore in cui metterò th e td per fare la tabella
+        let vett = new Array();
+
+        //per tutti i toDo
+        for(let i = 0; i < this.gest.length; i++)
+        {
+            //se è completato
+            if(this.gest[i].getIsCompletato() == true)
+            {
+                vett[i] = this.creaTabella(this.gest[i], i);
+            }
+        }
+
+        let table = document.getElementById("tblToDo");
+        //svuoto la tabella
+        table.innerHTML = "";
+
+        for(let i = 0;i < vett.length; i++)
+        {
+            table.innerHTML += vett[i];
+        }
+
+        //imposto il colore delle urgenze
+        this.impostaColore();
+    }
+
+    visualizzaNonCompletate()
+    {
+        //creo un vettore in cui metterò th e td per fare la tabella
+        let vett = new Array();
+
+        //per tutti i toDo
+        for(let i = 0; i < this.gest.length; i++)
+        {
+            //se è completato
+            if(this.gest[i].getIsCompletato() == false)
+            {
+                vett[i] = this.creaTabella(this.gest[i], i);
+            }
+        }
+
+        let table = document.getElementById("tblToDo");
+        //svuoto la tabella
+        table.innerHTML = "";
+
+        for(let i = 0;i < vett.length; i++)
+        {
+            table.innerHTML += vett[i];
+        }
+
+        //imposto il colore delle urgenze
+        this.impostaColore();
+    }
+
+    visualizzaTutte()
+    {
+        this.visualizzaToDoNellaTabella();
+    }
+
+    impostaColore()
+    {
+        let vett = [];
+
+        //per tutti i toDo
+        for(let i = 0; i< this.gest.length;i++)
+        {
+            //pos i del toDo == classe descrizione e bottone elimina del toDo
+            //prendo tutti gli elementi della classe
+            vett = document.getElementsByClassName(i);
+
+            //il colore lo assegno solo ai toDo non completati
+            //se il toDo non è stato completato
+            if(!this.gest[i].getIsCompletato())
+            {
+                //prendo i giorni mancanti
+                let gg = this.gest[i].getGiorniMancanti();
+
+                //se mancano meno di 3 giorni
+                if(gg < 3)
+                {
+                    //scritta rossa
+                    vett[0].style = "color: red;";
+                }
+                else if (gg > 3 && gg < 7)
+                {
+                    //dai 4 ai 6 giorni
+
+                    //scritta gialla
+                    vett[0].style = "color: yellow;";
+                }
+                else if(gg >= 7)
+                {
+                    //dai 7 giorni in poi
+
+                    //scritta verde
+                    vett[0].style = "color: green;";
+                }
+            }
+        }
     }
 
     //dal più piccolo al più grande
@@ -62,7 +171,7 @@ class CGestioneToDo
 
     visualizzaToDoNellaTabella()
     {
-        //creo un vettore con th e td per fare la tabella
+        //creo un vettore in cui metterò th e td per fare la tabella
         let vett = new Array();
 
         //per tutti i toDo
@@ -80,6 +189,9 @@ class CGestioneToDo
         {
             table.innerHTML += vett[i];
         }
+
+        //imposto il colore delle urgenze
+        this.impostaColore();
     }
 
     creaTabella(ToDo, pos)
@@ -87,15 +199,45 @@ class CGestioneToDo
         let str = "";
 
         str += "<tr>";
+        str += "<input type='checkbox' onclick=gest.toDoCompletato("+pos+");>"
         str += "<td class ='" + pos + "'>" + ToDo.getDescrizione(); + "</td>";
         str += "<td class ='" + pos + "'>" + "<button type='button' onclick = gest.eliminaToDo("+pos+");>ELIMINA</button>" + "</td>";
 
         return str;
     }
 
+    toDoCompletato(indice)
+    {
+        //indice della checkbox == indice del todo
+
+        //prendo dal vettore dei toDo, il toDo in posizione = indice
+        let toDo = this.gest[indice];
+
+        //cambio lo stato del completamento
+        toDo.setIsCompletato();
+
+        //indice toDo == classe descrizione e bottone elimina del toDo
+        //prendo tutti gli elementi della classe
+        let vett = document.getElementsByClassName(indice);
+
+        //cambio lo stile in base al completamento
+        if(toDo.getIsCompletato() == true)
+        {
+            //completato
+            //descrizione barrata
+            vett[0].style = "text-decoration: line-through;";
+        }
+        else
+        {
+            //non completato
+            //descrizione normale
+            vett[0].style = "";
+        }
+    }
+
     eliminaTodo(classe)
     {
-        //prendo gli elementi della classe
+        //prendo tutti gli elementi della classe
         let vett = document.getElementsByClassName(classe);
 
         //prendo la descrizione (pos 0) del todo da eliminare
@@ -114,4 +256,51 @@ class CGestioneToDo
         //ricarico la table
         this.visualizzaToDoNellaTabella();
     }
+
+    eliminaTuttaToDoList()
+    {
+        //svuoto il vettore di toDo
+        this.gest = [];
+
+        //prendo la tabella
+        let table = document.getElementById("tblToDo");
+        //svuoto la tabella
+        table.innerHTML = "";
+    }
+
+    visualizzaUrgentiNellaTabella()
+    {
+        //creo un vettore temporaneo
+        let vettTemp = [];
+
+        //per tutti i toDo
+        for(let i = 0; i < this.gest.length; i++)
+        {
+            //se è urgente
+            if(this.gest[i].getIsUrgente() == true)
+            {
+                //inserisco il toDo nel vettore
+                vettTemp.push(this.gest[i]);
+            }
+        }
+
+        //creo un vettore in cui metterò th e td per fare la tabella
+        let vett = new Array();
+
+        //per tutti i toDo urgenti
+        for(let i = 0; i < vettTemp.length; i++)
+        {
+            vett[i] = this.creaTabella(vettTemp[i], i);
+        }
+
+
+        let table = document.getElementById("tblToDo");
+        //svuoto la tabella
+        table.innerHTML = "";
+
+        for(let i = 0;i < vett.length; i++)
+        {
+            table.innerHTML += vett[i];
+        }
+    }    
 }
